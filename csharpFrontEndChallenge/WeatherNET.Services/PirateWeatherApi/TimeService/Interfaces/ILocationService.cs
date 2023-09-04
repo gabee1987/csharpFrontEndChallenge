fca918a71eedd217205a2170420e496a6a0ac1ae
Gabor Koncz1 <Gabor_Koncz1@epam.com>
2023-09-04 15:48:28 +0200

    created the skeletal for the location service

---
+++ csharpFrontEndChallenge/WeatherNET.Services/PirateWeatherApi/TimeService/Interfaces/ILocationService.cs
@@ -0,0 +1,7 @@
+﻿namespace WeatherNET.Services.PirateWeatherApi.TimeService.Interfaces
+{
+    public interface ILocationService
+    {
+        Task<string> GetLocationNameAsync( double latitude, double longitude );
+    }
+}
---
+++ csharpFrontEndChallenge/WeatherNET.Services/PirateWeatherApi/TimeService/LocationService.cs
@@ -0,0 +1,7 @@
+﻿namespace WeatherNET.Services.PirateWeatherApi.TimeService
+{
+    public  class LocationService
+    {
+
+    }
+}
