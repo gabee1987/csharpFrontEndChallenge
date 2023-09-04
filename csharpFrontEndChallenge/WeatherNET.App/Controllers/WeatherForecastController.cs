fd91a9bd9b0fae535534916aa1c7cfc490aae9f8
Gabor Koncz1 <Gabor_Koncz1@epam.com>
2023-09-04 11:45:58 +0200

    added the pirate weather api model classes, renamed the domain model base class

--- csharpFrontEndChallenge/WeatherNET.App/Controllers/WeatherForecastController.cs
+++ csharpFrontEndChallenge/WeatherNET.App/Controllers/WeatherForecastController.cs
@@ -6,10 +6,10 @@ namespace WeatherNET.Controllers
     public class WeatherForecastController : Controller
     {
         private readonly IConfiguration _configuration;
-        private readonly ILogger<WeatherForecast> _logger;
+        private readonly ILogger<WeatherForecastModel> _logger;
 
 
-        public WeatherForecastController( IConfiguration configuration, ILogger<WeatherForecast> logger )
+        public WeatherForecastController( IConfiguration configuration, ILogger<WeatherForecastModel> logger )
         {
             _configuration = configuration;
             _logger        = logger;
