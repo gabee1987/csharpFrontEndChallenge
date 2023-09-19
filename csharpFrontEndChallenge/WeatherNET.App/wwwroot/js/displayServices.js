064af45e79514efe2a4392eaf10c863645fc9f0d
Gabor Koncz1 <Gabor_Koncz1@epam.com>
2023-09-19 11:13:14 +0200

    added a mouse scroll feature to the hourly chart and redesigned the crollbar

---
+++ csharpFrontEndChallenge/WeatherNET.App/wwwroot/js/displayServices.js
@@ -0,0 +1,23 @@
+(function () {
+    function initializeHorizontalScroll(elementSelector) {
+        console.log("element: ", elementSelector);
+        console.log("Horizontal Scroll Service initialized...")
+        $(elementSelector).on('wheel', function (e) {
+            if (e.originalEvent.deltaY > 0) {
+                $(this).scrollLeft($(this).scrollLeft() + 55);
+            } else {
+                $(this).scrollLeft($(this).scrollLeft() - 55);
+            }
+            e.preventDefault();
+        });
+    }
+
+    // always initialize the scroll on document ready for #hourlyScroll
+    $(document).ready(function () {
+        initializeHorizontalScroll('#hourlyForecastScrollChart');
+    });
+
+    // Export function to use it for other elements in the future
+    window.initializeHorizontalScroll = initializeHorizontalScroll;
+
+})();
